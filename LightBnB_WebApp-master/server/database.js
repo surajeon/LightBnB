64e69eacd79933ccd221c71aa8976a8c4dc45b71
Sura Jeon <surajeon@gmail.com>
2021-05-28 01:18:19 +0000

    Updated search functions in database.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -126,13 +126,63 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      return result.rows;
-    })
-    .catch((err) => {
-      return err.message;
-    });
+    const queryParams = [];
+
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+
+    let searchQuery = [];    
+
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      searchQuery.push(` city LIKE $${queryParams.length}`);
+    }
+
+    if (options.owner_id) {
+      queryParams.push(options.owner_id);
+      searchQuery.push(` owner_id = $${queryParams.length}`);
+    }
+
+    if (options.minimum_price_per_night) {
+      queryParams.push(options.minimum_price_per_night);
+      searchQuery.push(` cost_per_night >= $${queryParams.length}`);
+    }
+
+    if (options.maximum_price_per_night) {
+      queryParams.push(options.maximum_price_per_night);
+      searchQuery.push(` cost_per_night <= $${queryParams.length}`);
+    }
+
+    if (options.minimum_rating) {
+      queryParams.push(options.minimum_rating);
+      searchQuery.push(` property_reviews.rating >= $${queryParams.length}`);
+    }
+
+    if (searchQuery.length !== 0) {
+      queryString += `WHERE ${searchQuery.join(' AND ')}`;
+    }
+  
+    queryParams.push(limit);
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+  
+    console.log(queryString, queryParams);
+  
+    return pool.query(queryString, queryParams).then((res) => res.rows);
+
+  // return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
+  //   .then((result) => {
+  //     return result.rows;
+  //   })
+  //   .catch((err) => {
+  //     return err.message;
+  //   });
 };
 exports.getAllProperties = getAllProperties;
 
@@ -147,5 +197,7 @@ const addProperty = function(property) {
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
+
+  
 }
 exports.addProperty = addProperty;
